1c7184a7ca178547dcefa896ef8e68d8822f6a94
t0xodile <thomas.stacey@gmail.com>
2026-02-11 16:47:19 +0100

    Updated to use the correct version of the MontoyaRequestResponse Object.

--- src/main/kotlin/BasicCheck.kt
+++ src/main/kotlin/BasicCheck.kt
@@ -11,18 +11,20 @@ internal class BasicCheck(name: String?) : Scan(name) {
 
     //this is where your scan logic goes
     override fun doScan(baseReq: ByteArray, service: IHttpService): MutableList<IScanIssue> {
-        val checkRequestResponse = Utilities.buildMontoyaReq(baseReq, service) //Easy way to build a montoya request so you can stop messing with the old version
-        checkRequestResponse.withHeader("X-Assured", "Assured")
 
-        val resp = Utilities.montoyaApi.http().sendRequest(checkRequestResponse)
+        val base = Utilities.buildMontoyaResp(request(service, baseReq)) //Easy way to build a montoya request so you can stop messing with the old version
 
-        if (resp.response().body().toString().contains("Assuredwashere")) {
-            report("X-Assured header reflection", "The X-Assured header randomly caused reflected content in the response body!", resp) //Reporing an issue is this easy too!
+        val checkRequest = base.request().withAddedHeader("X-Assured", "Assured")
+
+        val checkRequestResponse = request(checkRequest, false) //Second param is Force HP1?
+
+        if (checkRequestResponse.response().bodyToString().contains("Assured")) {
+            report("X-Assured header reflection", "The X-Assured header randomly caused reflected content in the response body!", checkRequestResponse) // Reporing an issue is this easy too!
         }
 
         //Check a settings
         if (Utilities.globalSettings.getBoolean("Print Responses")) {
-            BulkUtilities.out("Response was -> " + resp.response().toString())
+            BulkUtilities.out("Response was -> " + checkRequestResponse.response().toString())
         }
 
         return mutableListOf<IScanIssue>()
